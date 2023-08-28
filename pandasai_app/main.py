f9e7c0cfc817d073178d027047f085c04ab73b04
ma-raza <amjadraza24@gmail.com>
2023-08-28 12:09:11 +1000

    Migrated the app to PandasV1.x

--- pandasai_app/main.py
+++ pandasai_app/main.py
@@ -1,69 +1,112 @@
+
+# from langchain.llms import OpenAI
+from pandasai import SmartDataframe
+from pandasai.llm import OpenAI
+from pandasai.callbacks import StdoutCallback
+from pandasai.llm import OpenAI
 import streamlit as st
-from openai.error import OpenAIError
-import sys
+import pandas as pd
 import os
-sys.path.append(os.path.abspath('.'))
+from pandasai_app.components.faq import faq
 
-from pandasai_app.components.sidebar import sidebar
-from pandasai_app.utils import parse_csv, run_pandasai_openaiapi, clear_submit
+file_formats = {
+    "csv": pd.read_csv,
+    "xls": pd.read_excel,
+    "xlsx": pd.read_excel,
+    "xlsm": pd.read_excel,
+    "xlsb": pd.read_excel,
+}
 
-if __name__ == '__main__':
+def clear_submit():
+    """
+    Clear the Submit Button State
+    Returns:
 
-    st.set_page_config(
-        page_title="PANDASAI APP: Generative AI with Pandas",
-        page_icon="📖",
-        layout="wide",
-        initial_sidebar_state="expanded",)
-    st.header("📖 Pandasai App")
-    sidebar()
+    """
+    st.session_state["submit"] = False
 
-    # Upload the File
-    uploaded_file = st.file_uploader(
-        "Upload a csv file",
-        type=["csv"],
-        help="CSV Files are supported as of now!",
-        on_change=clear_submit,
-    )
 
-    index = None
-    doc = None
-    if uploaded_file is not None:
-        if uploaded_file.name.endswith(".csv"):
-            df = parse_csv(uploaded_file)
-            index = True
-        else:
-            raise ValueError("File type not supported!")
-        # text = text_to_docs(doc)
+@st.cache_data(ttl="2h")
+def load_data(uploaded_file):
+    try:
+        ext = os.path.splitext(uploaded_file.name)[1][1:].lower()
+    except:
+        ext = uploaded_file.split(".")[-1]
+    if ext in file_formats:
+        return file_formats[ext](uploaded_file)
+    else:
+        st.error(f"Unsupported file format: {ext}")
+        return None
 
-    query = st.text_area("Ask a question about the dataframe/csv uploaded", on_change=clear_submit)
-    with st.expander("Advanced Options"):
-        _verbose = st.checkbox("Show Details About Python Code generated")
-        _enforce_privacy = st.checkbox("Enforce Privacy About Personal Data")
 
-    button = st.button("Submit")
+st.set_page_config(page_title="PandasAI ", page_icon="🐼")
+st.title("🐼 PandasAI: Chat with CSV")
 
-    # On SUBMIT of the button
-    if button or st.session_state.get("submit"):
-        if not st.session_state.get("api_key_configured"):
-            st.error("Please configure your OpenAI API key!")
-        elif not index:
-            st.error("Please upload a data file!")
-        elif not query:
-            st.error("Please enter a question!")
-        else:
-            st.session_state["submit"] = True
-            # Output Columns
-            query_col, answer_col = st.columns(2)
-            # sources = search_docs(index, query)
-            try:
-                answer = run_pandasai_openaiapi(df=df, prompt=query, verbose=False)
+uploaded_file = st.file_uploader(
+    "Upload a Data file",
+    type=list(file_formats.keys()),
+    help="Various File formats are Support",
+    on_change=clear_submit,
+)
 
-                with query_col:
-                    st.markdown("#### Query")
-                    st.markdown(query)
-                with answer_col:
-                    st.markdown("#### Answer")
-                    st.markdown(answer)
+if uploaded_file:
+    df = load_data(uploaded_file)
 
-            except OpenAIError as e:
-                st.error(e._message)
+openai_api_key = st.sidebar.text_input("OpenAI API Key",
+                                        type="password",
+                                        placeholder="Paste your OpenAI API key here (sk-...)",
+                                        help="You can get your API key from https://platform.openai.com/account/api-keys.")  # noqa: E501)
+
+with st.sidebar:
+        st.markdown("---")
+        st.markdown(
+            "## How to use\n"
+            "1. Enter your [OpenAI API key](https://platform.openai.com/account/api-keys) below🔑\n"  # noqa: E501
+            "2. Upload a csv file with data📄\n"
+            "3. A csv file is read as Pandas Dataframe📄\n"
+            "4. Ask a question about to make dataframe conversational💬\n"
+        )
+
+        st.markdown("---")
+        st.markdown("# About")
+        st.markdown(
+            "📖Pandasai App allows you to ask questions about your "
+            "csv / dataframe and get accurate answers"
+        )
+        st.markdown(
+            "Pandasai is in active development so is this tool."
+            "You can contribute to the project on [GitHub]() "  # noqa: E501
+            "with your feedback and suggestions💡"
+        )
+        st.markdown("Made by [DR. AMJAD RAZA](https://www.linkedin.com/in/amjadraza/)")
+        st.markdown("---")
+
+        faq()
+
+if "messages" not in st.session_state or st.sidebar.button("Clear conversation history"):
+    st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+for msg in st.session_state.messages:
+    st.chat_message(msg["role"]).write(msg["content"])
+
+if prompt := st.chat_input(placeholder="What is this data about?"):
+    st.session_state.messages.append({"role": "user", "content": prompt})
+    st.chat_message("user").write(prompt)
+
+    if not openai_api_key:
+        st.info("Please add your OpenAI API key to continue.")
+        st.stop()
+
+    #PandasAI OpenAI Model
+    llm = OpenAI(api_token=openai_api_key)
+    # llm = OpenAI(api_token=openai_api_key)
+
+    sdf = SmartDataframe(df, config = {"llm": llm,
+                                        "enable_cache": False,
+                                        "conversational": True,
+                                        "callback": StdoutCallback()})
+
+    with st.chat_message("assistant"):
+        response = sdf.chat(st.session_state.messages)
+        st.session_state.messages.append({"role": "assistant", "content": response})
+        st.write(response)
